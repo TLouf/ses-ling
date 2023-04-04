6b1a988b2e075c2bda598166994cf9c735a7aeca
TLouf <loufthomas@gmail.com>
2023-04-04 12:09:35 +0200

    Allow any number of columns in assort_mosaic

--- ses_ling/visualization/ses.py
+++ ses_ling/visualization/ses.py
@@ -1,23 +1,24 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 import ses_ling.visualization.utils as viz_utils
 
 
-def assort_mosaic(assort_dict, nr_cols, figsize, log_scale=False):
-    mosaic = viz_utils.prep_mosaic_from_dict(assort_dict, nr_cols=2)
+def assort_mosaic(assort_dict, nr_cols, figsize, log_scale=False, **subplot_kwargs):
+    mosaic = viz_utils.prep_mosaic_from_dict(assort_dict, nr_cols=nr_cols)
+    nr_rows = mosaic.shape[0]
+    mosaic = np.column_stack((['supy'] * nr_rows, mosaic, ['cax'] * nr_rows))
     fig, axd = plt.subplot_mosaic(
-        mosaic, sharex=True, sharey=True, figsize=figsize,
-        constrained_layout=True,
+        mosaic, figsize=figsize,
+        constrained_layout=True, **subplot_kwargs
     )
+    axd['supy'].set_axis_off()
     assort_mins, assort_maxs = zip(*[
         (city_dict['assort'].values.min(), city_dict['assort'].values.max()) 
         for city_dict in assort_dict.values()
     ])
     for i, (city, city_dict) in enumerate(assort_dict.items()):
         ax = axd[city]
-        cbar = i == 0
-        pearsonr = city_dict['pearsonr']
         assort_plot = city_dict['assort'].T[::-1].copy()
         vmin = min(assort_mins)
         vmax = min(assort_maxs)
@@ -26,13 +27,20 @@ def assort_mosaic(assort_dict, nr_cols, figsize, log_scale=False):
             vmax = np.log10(vmax)
             vmin = np.log10(vmin)
         im = ax.imshow(assort_plot, cmap='Blues', vmin=vmin, vmax=vmax)
-        ax.set_title(f"{city}, r = {pearsonr:.2f}")
+        ax.set_title(f"{city}")#, r = {pearsonr:.2f}")
+        spec = ax.get_subplotspec()
+        if spec.colspan.start >= 2:
+            ax.sharey(axd[mosaic[spec.rowspan.start, 1]])
+            ax.tick_params(labelleft=False)
+        if spec.rowspan.start < nr_rows - 1:
+            ax.sharex(axd[mosaic[-1, spec.colspan.start]])
+            ax.tick_params(labelbottom=False)
 
-    nr_classes = assort_plot.shape[0]
-    ax.set_xticks(np.arange(nr_classes), assort_plot.columns.astype(str))
-    ax.set_yticks(np.arange(nr_classes), assort_plot.index.astype(str))
+        nr_classes = assort_plot.shape[0]
+        ax.set_xticks(np.arange(nr_classes), assort_plot.columns.astype(str))
+        ax.set_yticks(np.arange(nr_classes), assort_plot.index.astype(str))
     fig.supxlabel('SES class of residence')
-    fig.supylabel('SES class of visited places')
-    cbar_label = 'log(probability)' if log_scale else 'probability'
-    fig.colorbar(im, ax=list(axd.values()), shrink=0.6, label=cbar_label)
+    fig.supylabel('SES class of visited places', x=0)
+    cbar_label = 'log(proportion of trips)' if log_scale else 'proportion of trips'
+    fig.colorbar(im, cax=axd['cax'], label=cbar_label)
     return fig, axd
