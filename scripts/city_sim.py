7eb82fed4fb2944dea892f316db4a909acb3b196
TLouf <loufthomas@gmail.com>
2023-04-14 10:37:17 +0200

    Add `city_sim` script

---
+++ scripts/city_sim.py
@@ -0,0 +1,22 @@
+import sys
+
+import ses_ling.utils.sim as sim_utils
+
+if __name__ == '__main__':
+    lv = float(sys.argv[1])
+    q1 = float(sys.argv[2])
+    q2 = float(sys.argv[3])
+    cells_nr_users_th = int(sys.argv[4])
+    nr_steps = int(sys.argv[5])
+
+    focus_cities = [
+        'London', 'Manchester', 'Birmingham', 'Liverpool', 'Leeds', 'Sheffield', 
+        'Newcastle upon Tyne', 'Bristol, City of'
+    ]
+    for city in focus_cities:
+        sim = sim_utils.Simulation.from_saved_state(
+            city, lv=lv, q1=q1, q2=q2, cells_nr_users_th=cells_nr_users_th
+        )
+        print(sim.to_dict())
+        sim.run(nr_steps)
+        sim.save_state()
