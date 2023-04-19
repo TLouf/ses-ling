be54e00da14ab65460e27e3d72d58b24a7f38d6e
TLouf <loufthomas@gmail.com>
2023-04-19 17:15:58 +0200

    Add get_multiidx_at pandas util

--- ses_ling/utils/pandas.py
+++ ses_ling/utils/pandas.py
@@ -5,3 +5,6 @@ def empy_multiidx(names):
     empty_vals = [[]] * len(names)
     empty_idx = pd.MultiIndex(codes=empty_vals, levels=empty_vals, names=names)
     return empty_idx
+
+def get_multiidx_at(df, n):
+    return df.iloc[[n]].index.to_frame(index=False).to_dict(orient='index')[0]
