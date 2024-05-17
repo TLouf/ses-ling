80949a84f77f01290f3a18c26248ff2c021bf300
TLouf <loufthomas@gmail.com>
2024-05-17 10:36:06 +0200

    Allow single colorbar in `assort_mosaic`

--- ses_ling/visualization/ses.py
+++ ses_ling/visualization/ses.py
@@ -1,18 +1,31 @@
+import re
+
 import matplotlib.pyplot as plt
 import numpy as np
+from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
 
 import ses_ling.visualization.utils as viz_utils
 
 
-def assort_mosaic(assort_dict, nr_cols, figsize, log_scale=False, show_pearson=False, **subplot_kwargs):
+def assort_mosaic(assort_dict, nr_cols, figsize, log_scale=False, show_pearson=False, single_cbar=True, **subplot_kwargs):
+    cbar_label = 'log(percentage of trips)' if log_scale else 'percentage of trips'
     mosaic = viz_utils.prep_mosaic_from_dict(assort_dict, nr_cols=nr_cols)
     nr_rows = mosaic.shape[0]
-    mosaic = np.column_stack((['supy'] * nr_rows, mosaic, ['cax'] * nr_rows))
+    if single_cbar:
+        mosaic = [['supy'] * nr_rows, mosaic] + [['cax'] * nr_rows]
+    else:
+        mosaic = np.insert(mosaic, np.arange(nr_cols + 1)[1:], 'cax', axis=1).tolist()
+        for i in range(nr_rows):
+            for j in range(nr_cols):
+                mosaic[i][2*j+1] = f'{mosaic[i][2*j+1]}_{mosaic[i][2*j]}'
+        mosaic = [['left_supy'] * nr_rows, mosaic, ['right_supy'] * nr_rows]
+    mosaic = np.column_stack(mosaic)
     fig, axd = plt.subplot_mosaic(
         mosaic, figsize=figsize,
-        constrained_layout=True, **subplot_kwargs
+        layout='compressed', **subplot_kwargs
     )
-    axd['supy'].set_axis_off()
+    axd['left_supy'].set_axis_off()
+    axd['right_supy'].set_axis_off()
     assort_mins, assort_maxs = zip(*[
         (city_dict['assort'].values.min(), city_dict['assort'].values.max()) 
         for city_dict in assort_dict.values()
@@ -20,15 +33,22 @@ def assort_mosaic(assort_dict, nr_cols, figsize, log_scale=False, show_pearson=F
     for i, (city, city_dict) in enumerate(assort_dict.items()):
         ax = axd[city]
         pearsonr = city_dict['pearsonr']
+        # x axis residence, y axis destination
         assort_plot = city_dict['assort'].T[::-1].copy()
-        vmin = min(assort_mins)
-        vmax = max(assort_maxs)
+        vmin = min(assort_mins) if single_cbar else assort_mins[i]
+        vmax = max(assort_maxs) if single_cbar else assort_maxs[i]
         if log_scale:
             assort_plot = np.log10(assort_plot)
             vmax = np.log10(vmax)
             vmin = np.log10(vmin)
         im = ax.imshow(assort_plot, cmap='Blues', vmin=vmin, vmax=vmax)
-        ax.set_title(city + show_pearson * f", r = {pearsonr:.2f}")
+        short_city_name = re.split(r'\W+', city)[0]
+        pearson_str = f"\n$r_M = {pearsonr:.2f}$"
+        ax.set_title(
+            short_city_name + show_pearson * pearson_str,
+            # usetex=True,
+            math_fontfamily='cm',
+        )
         spec = ax.get_subplotspec()
         if spec.colspan.start >= 2:
             ax.sharey(axd[mosaic[spec.rowspan.start, 1]])
@@ -40,8 +60,20 @@ def assort_mosaic(assort_dict, nr_cols, figsize, log_scale=False, show_pearson=F
         nr_classes = assort_plot.shape[0]
         ax.set_xticks(np.arange(nr_classes), assort_plot.columns.astype(str))
         ax.set_yticks(np.arange(nr_classes), assort_plot.index.astype(str))
-    fig.supxlabel('SES class of residence')
-    fig.supylabel('SES class of visited places', x=0)
-    cbar_label = 'log(proportion of trips)' if log_scale else 'proportion of trips'
-    fig.colorbar(im, cax=axd['cax'], label=cbar_label)
+        if not single_cbar:
+            # ax_divider = make_axes_locatable(ax)
+            # # Add an Axes to the right of the main Axes.
+            # cax = ax_divider.append_axes("right", size="5%", pad="4%")
+            cax = axd[f'cax_{city}']
+            fig.colorbar(im, cax=cax)
+
+    fig.supxlabel('SES class of residence', fontsize='large')
+    # fig.supylabel('SES class of residence', x=0)
+    axd['left_supy'].annotate(
+        'SES class of visited places', (0, 0.47),
+        va='center', rotation='vertical', fontsize='large'
+    )
+    axd['right_supy'].annotate(cbar_label, (0, 0.47), va='center', rotation='vertical')
+    if single_cbar:
+        fig.colorbar(im, cax=axd['cax'], label=cbar_label)
     return fig, axd
